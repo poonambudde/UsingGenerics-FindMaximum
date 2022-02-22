5d3608c0d2f52892f4be49c03c6e7e7ca4f2077c
poonambudde <buddepoonam123@gmail.com>
2022-02-22 21:18:03 +0530

    [Poonam] ADDED: Welcome to Find Max using Generics

---
+++ Maximum Generics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Maximum_Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Find Maximum of three numbers using Generics Programs");
+        }
+    }
+}
