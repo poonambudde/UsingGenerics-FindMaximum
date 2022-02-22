7e84659abf90009b34e92f0c4fe19ab9fc2b0ad3
poonambudde <buddepoonam123@gmail.com>
2022-02-22 21:24:23 +0530

    [Poonam] ADDED: Find the maximum of Given 3 Integers

---
+++ Maximum Generics/FindMaximum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maximum_Generics
+{
+    class FindMaximum
+    {
+        public int findMaxIntValue(int firstValue, int secondValue, int thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            else if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            else
+            {
+                return thirdValue;
+            }
+        }
+    }
+}
--- Maximum Generics/Program.cs
+++ Maximum Generics/Program.cs
@@ -7,6 +7,9 @@ namespace Maximum_Generics
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Find Maximum of three numbers using Generics Programs");
+
+            FindMaximum findMaximum = new FindMaximum();
+            Console.WriteLine("Maximum Number is: " + findMaximum.findMaxIntValue(45, 89, 67));
         }
     }
 }
