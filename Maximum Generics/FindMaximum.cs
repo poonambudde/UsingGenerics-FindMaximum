21b22d03d00a022b767c394626206d6278a064a2
poonambudde <buddepoonam123@gmail.com>
2022-02-23 05:23:24 +0530

    [Poonam] REFACTOR: create Generic Class to take in 3 variables of Generic Type

---
+++ Maximum Generics/FindMaximum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maximum_Generics
+{
+    class FindMaximum<T>
+    {
+        public T findMaxValue(T firstValue, T secondValue, T thirdValue)
+        {
+            if (Comparer<T>.Default.Compare(firstValue, secondValue) > 0 && Comparer<T>.Default.Compare(firstValue, thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            else if (Comparer<T>.Default.Compare(secondValue, firstValue) > 0 && Comparer<T>.Default.Compare(secondValue, thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            else
+            {
+                return thirdValue;
+            }
+        }
+    }
+}
--- Maximum Generics/Program.cs
+++ Maximum Generics/Program.cs
@@ -1,36 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Maximum_Generics
 {
-    internal class FindMaximum
+    class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Find Maximum of three numbers using Generics Programs");
 
-            FindMaximum findMaximum = new FindMaximum();
-            Console.WriteLine("Maximum Integer value is : " + findMaximum.findMaxValue<int>(599, 149, 853));
-            Console.WriteLine("Maximum Floating value is : " + findMaximum.findMaxValue<double>(9.3, 6.5, 5.8));
-            Console.WriteLine("Maximum String is : " + findMaximum.findMaxValue<string>("Apple", "Mango", "Banana"));
+            FindMaximum<int> findIntMax = new FindMaximum<int>();
+            FindMaximum<double> findMaximumDouble = new FindMaximum<double>();
+            FindMaximum<string> findstring = new FindMaximum<string>();
+
+            Console.WriteLine("Maximum of Integer value is : " + findIntMax.findMaxValue(599, 482, 635));
+            Console.WriteLine("Maximum Floating value is : " + findMaximumDouble.findMaxValue(304.45, 95.27, 467.18));
+            Console.WriteLine("Maximum String is : " + findstring.findMaxValue("Apricot", "Banana", "Kiwi"));
+
         }
-        public T findMaxValue<T>(T firstValue, T secondValue, T thirdValue)
-        {
-            if (Comparer<T>.Default.Compare(firstValue, secondValue) > 0 && Comparer<T>.Default.Compare(firstValue, thirdValue) > 0)
-            {
-                return firstValue;
-            }
-            else if (Comparer<T>.Default.Compare(secondValue, firstValue) > 0 && Comparer<T>.Default.Compare(secondValue, thirdValue) > 0)
-            {
-                return secondValue;
-            }
-            else
-            {
-                return thirdValue;
-            }
-        }
+
     }
 }
 
