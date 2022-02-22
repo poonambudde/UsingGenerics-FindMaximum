4bf723f9c96f869dea0daf7849edf0116c8000a4
poonambudde <buddepoonam123@gmail.com>
2022-02-22 22:38:16 +0530

    [Poonam] ADDED: Find the maximum of Given 3 Strings

--- Maximum Generics/Program.cs
+++ Maximum Generics/Program.cs
@@ -9,7 +9,7 @@ namespace Maximum_Generics
             Console.WriteLine("Welcome to Find Max of three numbers using generics Programs");
 
             FindMaximum findMaximum = new FindMaximum();
-            Console.WriteLine("Maximum of three floating values is: " + findMaximum.findMaxDoubleValue(8.9, 5.7, 3.4));
+            Console.WriteLine("Maximum of three Strings is: " + findMaximum.findMaxStringValue("Peach", "Apple", "Banana"));
         }
     }
 }
