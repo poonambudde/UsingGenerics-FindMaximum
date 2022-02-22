0e85af614098af92c88c2c4f62183eed7badcb72
poonambudde <buddepoonam123@gmail.com>
2022-02-23 04:55:18 +0530

    [Poonam] REFACTOR: Find the Maximum refactor all 3 to One Generic Method

--- Maximum Generics/Program.cs
+++ Maximum Generics/Program.cs
@@ -1,15 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Maximum_Generics
 {
-    class Program
+    internal class FindMaximum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Find Max of three numbers using generics Programs");
+            Console.WriteLine("Welcome to Find Maximum of three numbers using Generics Programs");
 
             FindMaximum findMaximum = new FindMaximum();
-            Console.WriteLine("Maximum of three Strings is: " + findMaximum.findMaxStringValue("Peach", "Apple", "Banana"));
+            Console.WriteLine("Maximum Integer value is : " + findMaximum.findMaxValue<int>(599, 149, 853));
+            Console.WriteLine("Maximum Floating value is : " + findMaximum.findMaxValue<double>(9.3, 6.5, 5.8));
+            Console.WriteLine("Maximum String is : " + findMaximum.findMaxValue<string>("Apple", "Mango", "Banana"));
+        }
+        public T findMaxValue<T>(T firstValue, T secondValue, T thirdValue)
+        {
+            if (Comparer<T>.Default.Compare(firstValue, secondValue) > 0 && Comparer<T>.Default.Compare(firstValue, thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            else if (Comparer<T>.Default.Compare(secondValue, firstValue) > 0 && Comparer<T>.Default.Compare(secondValue, thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            else
+            {
+                return thirdValue;
+            }
         }
     }
 }
+
